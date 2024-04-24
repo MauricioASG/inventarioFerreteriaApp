b7ba4c7870c09cf2c606d55dc31940e1a85eb10c
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-04-24 13:32:32 -0600

    home page works

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { RouterProp } from '@react-navigation/native';
 import { StackNavigationProp } from "@react-navigation/stack";
 import React, { useEffect } from "react";
-import { Button, SafeAreaView, StyleSheet, Text, Touchable } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, Touchable, View } from "react-native";
 import { Product } from '../../model/Product';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 
@@ -21,8 +21,19 @@ function Home({ navigation }: HomeProps): React.JSX.Element {
   const [products, setProducts] = React.useState<Product[]>([]);
 const productItem = ({item}: {item: Product}) => (
   <TouchableOpacity style={styles.productItem}>
-    <Text style={styles.itemTitle}>{item.nombre}</Text>
-  </TouchableOpacity>
+    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+      <View style={{flexDirection: 'column'}}>
+      <Text style={styles.itemTitle}>{item.nombre}
+      </Text>
+      </View>  
+      <Text style={styles.itemDetails}>Precio: ${item.precio.toFixed(2)}</Text>
+    </View>
+    <Text
+    style={[styles.itemBadge, item.currentStock < item.minStock ? styles.itemBadgeError: null
+    ]}>
+    {item.currentStock}
+    </Text>
+    </TouchableOpacity>
   );
 
   useEffect(() => {
@@ -39,16 +50,32 @@ const productItem = ({item}: {item: Product}) => (
 }
 
 const styles = StyleSheet.create({
+  itemBadge: {
+    fontSize: 24,
+    color: 'black',
+    alignSelf: 'flex-end',
+  },
+  itemBadgeError: {
+    fontSize: 24,
+    color: 'red',
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+  },
   productItem:{
     padding: 12,
+    borderBlockColor: '#c0c0c0',
+    borderBottomWidth: 1,
+    backgroundColor: 'white',
   },
   itemTitle: {
-    fontSize: 20,
+    fontSize: 24,
     color: '#000',
+    textTransform: 'uppercase',
   },
   itemDetails:{
     fontSize: 14,
-    opacity: 0.7
+    opacity: 0.7,
+    color: '#000',
   }
 });
 export default Home;
